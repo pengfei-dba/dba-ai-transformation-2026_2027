bf3e402eb853e6796fd526bff49685678963a95b
泡泡君 <975261516@qq.com>
2026-06-29 21:13:27 +0800

    test_env

--- 01-python-automation/m1-slow-query-catcher/db_test.py
+++ 01-python-automation/m1-slow-query-catcher/db_test.py
@@ -2,3 +2,6 @@
 connection={"host":"127.0.0.1","port":3306,"user":"root"}
 conn_str=f"connecting to {connection['user']}@{connection['host']}:{connection['port']}"
 print(conn_str)
+
+for key,value in connection.items():
+     print(key,value)
